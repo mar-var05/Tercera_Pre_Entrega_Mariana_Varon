902f257dffd88f756f28cfc53192dce2eebc5ee7
Mariana Varon <msofiavyopasa@gmail.com>
2023-06-05 14:10:49 -0500

    Creacion de proyecto Django y aplicacion inicio

--- Entrega3/config/settings.py
+++ Entrega3/config/settings.py
@@ -10,10 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+import sys
+APLICACIONES = BASE_DIR/ "Aplicaciones"
+sys.path.append(str(APLICACIONES))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inicio',
 ]
 
 MIDDLEWARE = [
