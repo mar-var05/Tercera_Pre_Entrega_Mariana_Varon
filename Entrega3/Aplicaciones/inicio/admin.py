3b834365d51493329de6ade63a78c2c8a9289522
Mariana Varon <msofiavyopasa@gmail.com>
2023-06-05 17:00:46 -0500

    Update admin de inicio, se registaron los models

--- Entrega3/Aplicaciones/inicio/admin.py
+++ Entrega3/Aplicaciones/inicio/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Clientes)
+admin.site.register(models.Mascota)
+admin.site.register(models.Servicio)
--- Entrega3/Aplicaciones/inicio/models.py
+++ Entrega3/Aplicaciones/inicio/models.py
@@ -9,16 +9,24 @@ class Clientes(models.Model):
     correo = models.EmailField()
     numero_movil = models.CharField(max_length=15)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Mascota(models.Model):
     nombre_mascota = models.CharField(max_length=15)
     especie = models.CharField(max_length=10)
-    raza = models.FloatField(max_length=15)
+    raza = models.CharField(max_length=15)
     edad = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre_mascota
 
 
 class Servicio(models.Model):
-    tipo_de_servicio = models.CharField(max_length=10)
-    descripcion_del_servicio = models.CharField(max_length=30)
+    tipo_de_servicio = models.CharField(max_length=20)
     servicio_completado = models.BooleanField()
-    
+
+    def __str__(self):
+        return self.tipo_de_servicio
+
